42c5f33e9be9776f59ced73abaad4680637ade18
Byung Kyu Kim <cdeclare@gmail.com>
2020-02-18 18:35:08 +0900

    feat: add name argument

--- pyexpr.py
+++ pyexpr.py
@@ -8,44 +8,40 @@ def get_query(path, query, execute, match, debug):
 	url = path
 	payload = json.dumps(query)
 	headers = {'content-type': 'application/json'}
-	response = requests.get(url, data=payload, headers=headers)
+	response = requests.get(url, data=payload, headers=headers) 
 	expr = response.json()
-	
 	if not match: match = 'True'
 	if not debug: debug = 'expr'
 	if execute: exec(execute)
 
 	return (eval(match), eval(debug))
 
-# def filequery(path, execute, query, match, debug):
-# 	with open(path, encoding='utf-8') as fin:
-# 		txt = fin.read()
-# 		regex = re.compile(query)
-# 		expr = regex.findall(txt, re.M)
-# 		if not debug: debug = 'expr'
-# 		exec(execute)
-# 		return (eval(match), eval(debug))
-
 handler = {}
 handler['http'] = get_query
 
 def parse(f, args):
 	conf = yaml.safe_load(f)
-
-	for c in conf["tasks"]:
+	
+	for c in conf['tasks']:
 		task = c['task']
 		if not task in handler: continue
 
+		name = c['name']
+		if args.name and args.name != name: continue
+
 		if args.verbose:
-			print(c)
+			print('verbose {}'.format('-' * 80))
+			print(json.dumps(c, indent=4))
 		
-		(ok, debug) = handler[task](c["path"], c["query"], c["execute"], c["match"], c["debug"])
-		print('ok : {}'.format(ok))
-		print('debug : {}'.format(debug))
+		(fire, debug) = handler[task](c["path"], c["query"], c["execute"], c["match"], c["debug"])
+		print('output {}'.format('-' * 80))
+		print('[fire] : {}'.format(fire))
+		print('[debug] : {}'.format(debug))
 		print()
 
-def runConfig(args):
-	with open('conf.yml', 'r', encoding='utf-8') as f:
+def runYaml(args):
+	fname = args.file
+	with open(fname, 'r', encoding='utf-8') as f:
 		try:
 			parse(f, args)
 		except Exception as e:
@@ -54,12 +50,14 @@ def runConfig(args):
 def usage():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--verbose', '-v', help="verbose", action='store_true')
+	parser.add_argument('--file', '-f', help="yaml file ", type=str, default="tasks.yml")
+	parser.add_argument('--name', '-n', help="task name", type=str, default="")
 	args = parser.parse_args()
 	return args
 
 def main():
 	args = usage()
-	runConfig(args)
+	runYaml(args)
 
 if __name__ == "__main__":
 	main()
