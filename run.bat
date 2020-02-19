9f79f46646d501b8a252f4a4575844a477370680
Byung Kyu Kim <cdeclare@gmail.com>
2020-02-19 09:50:04 +0900

    clean up

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 
-type name.txt | bin\parun.exe -p 8 venv\scripts\python pyexpr.py -v -n
+type name.txt | bin\parun.exe -p 8 venv\scripts\python pyexpr.py -v -f conf\tasks.yml -n
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat name.txt | bin/parun -p 8 venv/bin/python pyexpr.py -v -n
+cat name.txt | bin/parun -p 8 venv/bin/python pyexpr.py -v -f conf/tasks.yml -n
